fd7cf7c68a38101d7a2e1bee7da4a50b76d7555f
mkarawacki <mapkarawacki@gmail.com>
2021-10-23 00:10:05 +0200

    essential comments added + first attempts at iterative generation of map plots for all periods studied

--- netcdf_reader.py
+++ netcdf_reader.py
@@ -57,7 +57,7 @@ icecov_fr_1=np.mean(ice_1)
 
 fig = plt.figure(600)
 
-ax = fig.add_subplot(141)
+ax = fig.add_subplot(941)
 
 ax.set_title("Ice coverage")
 m = Basemap(projection='moll',lat_0=0,lon_0=0)
@@ -65,38 +65,38 @@ m = Basemap(projection='moll',lat_0=0,lon_0=0)
 lon, lat = np.meshgrid(lons, lats)
 xi, yi = m(lon, lat)
 m.drawcoastlines()
-cs = m.pcolor(xi,yi,ice_1[:])
+cs = m.pcolor(xi,yi,ice_1[:],cmap='jet')
 cbar = m.colorbar(cs, location='bottom', pad="10%")
 
 
-ax = fig.add_subplot(142)
+ax = fig.add_subplot(942)
 ax.set_title("Upwelling")
 m = Basemap(projection='moll',lat_0=0,lon_0=0)
 m.drawcoastlines()
 lon, lat = np.meshgrid(lons, lats)
 xi, yi = m(lon, lat)
 
-cs = m.pcolor(xi,yi,np.squeeze(upwelling_1[:]))
+cs = m.pcolor(xi,yi,np.squeeze(upwelling_1[:]),cmap='jet')
 cbar = m.colorbar(cs, location='bottom', pad="10%")
 
-ax = fig.add_subplot(143)
+ax = fig.add_subplot(943)
 ax.set_title("Wind stress")
 m = Basemap(projection='moll',lat_0=0,lon_0=0)
 m.drawcoastlines()
 lon, lat = np.meshgrid(lons, lats)
 xi, yi = m(lon, lat)
 
-cs = m.pcolor(xi,yi,np.squeeze(tau_1[:]))
+cs = m.pcolor(xi,yi,np.squeeze(tau_1[:]),cmap='jet')
 cbar = m.colorbar(cs, location='bottom', pad="10%")
 
-ax = fig.add_subplot(144)
+ax = fig.add_subplot(944)
 ax.set_title("Mixing layer depth")
 m = Basemap(projection='moll',lat_0=0,lon_0=0)
 m.drawcoastlines()
 lon, lat = np.meshgrid(lons, lats)
 xi, yi = m(lon, lat)
 
-cs = m.pcolor(xi,yi,oc_vars_1['oij_mld'][:])
+cs = m.pcolor(xi,yi,oc_vars_1['oij_mld'][:],cmap='jet')
 cbar = m.colorbar(cs, location='bottom', pad="10%")
 
 
@@ -110,35 +110,35 @@ icecov=[icecov_fr_1]
 
 #periods=list(filter(reg.search,aij_list)) 
 periods= [4,8,16,32,64,128,243,256]
+i=4
 for p in periods:
-    reg=re.compile('_'+str(p)).search
-    #print(p,list(filter(reg,aij_list))[0],list(filter(reg,oij_list))[0],list(filter(reg,oijl_list))[0])
-    atmo = netCDF4.Dataset(list(filter(reg,aij_list))[0])
-    ocean = netCDF4.Dataset(list(filter(reg,oij_list))[0])
-    oijl = netCDF4.Dataset(list(filter(reg,oijl_list))[0])
-    atmo_vars=atmo.variables
-    oc_vars=ocean.variables
-    oijl_vars = oijl.variables
-    oclayers=np.linspace(0,5000,13) # ocean layers
+    reg=re.compile('_'+str(p)).search #Regular Expression looking for string matching period duration in .nc filenames
+    atmo = netCDF4.Dataset(list(filter(reg,aij_list))[0]) #read AIJ .nc file with given period
+    ocean = netCDF4.Dataset(list(filter(reg,oij_list))[0]) #read OIJ .nc file with given period
+    oijl = netCDF4.Dataset(list(filter(reg,oijl_list))[0]) #read OIJL .nc file with given period
+    atmo_vars=atmo.variables # dict of AIJ table variables
+    oc_vars=ocean.variables # dict of OIJ table variables
+    oijl_vars = oijl.variables# dict of OIJL table variables
+    oclayers=np.linspace(0,5000,13) # ocean layers - obsolete thus far
     
    
-    wind_u=atmo_vars['usurf']
-    wind_v=atmo_vars['vsurf']
-    wind_vel=np.sqrt(np.power(wind_u[:],2)+np.power(wind_v[:],2))
+    wind_u=atmo_vars['usurf'] #u component of surface wind velocity
+    wind_v=atmo_vars['vsurf'] #v component of surface wind velocity
+    wind_vel=np.sqrt(np.power(wind_u[:],2)+np.power(wind_v[:],2)) # wind velocity as sqrt(u^2 + v^2)
 
-    snow_cov_per=atmo_vars['snowicefr']
-    land=atmo_vars['frac_land']
+    snow_cov_per=atmo_vars['snowicefr'] #snow ince fraction
+    land=atmo_vars['frac_land'] #land fraction
     
-    temp_surf=atmo_vars['tsurf']
-    p_surf=atmo_vars['prsurf']
-    ice=snow_cov_per[:]-land[:]
+    temp_surf=atmo_vars['tsurf'] #surface temperature - obsolete
+    p_surf=atmo_vars['prsurf'] #surface pressure - obsolete
+    ice=snow_cov_per[:]-land[:] # masking out land from snow coverage map
     ice[ice<0]=0
-    icecov_fr=np.mean(ice)
+    icecov_fr=np.mean(ice) # global average ice coverage fraction
     icecov.append(icecov_fr)
 
-    oc_mixl=oc_vars['oij_mld'][:]
-    rho_surf = p_surf[:]/(temp_surf[:]*R/Mmol)
-    #tau=C_d*rho_surf*np.power(wind_vel[:],2)
+    oc_mixl=oc_vars['oij_mld'][:] # ocean mixing layer depth [m]
+    # rho_surf = p_surf[:]/(temp_surf[:]*R/Mmol)
+    # tau=C_d*rho_surf*np.power(wind_vel[:],2) -- obsolete substitution of ideal gas law into wind stress formula
     tau=oc_vars['oij_ustar'] # windstress
     
     pot_dens=oijl_vars['pot_dens']
@@ -153,62 +153,7 @@ for p in periods:
     tau_arr.append(np.mean(tau)/np.mean(tau_1))
     oc_mixl=np.mean(oc_vars['oij_mld'][:])
     mld.append(oc_mixl)
+    i=i+1
+    ax = fig.add_subplot(9,4,4+i)
+    
 
-
-'''
-
-Hi Mike! 
-
-That sounds like a fun project! Happy to help. 
-
-The mixed layer depth (oij_mld) 
-was unmodified and should be straightforward. 
-
-
-Sea ice was only slightly modified. 
-I just calculated sea ice cover with snowicefr (aij) 
-after applying a land mask. 
-
-I suppose oicefr (also aij) would be easier if you don’t mind some lakes. 
-
-The others panels in Figure 7 required some simple calculations that use: 
-    surface windspeed (wsurf, aij), potential density (pot_dens, oijl), 
-    and downward vertical velocity (w, oijl). 
-The equations are provided in Section 2.4 and for the calculations are fairly straightforward, 
-the possible exception being upwelling. 
-
-For the upwelling calculation, 
-I wrote a silly script that looped through the i,j grid and found the ocean level
-that contained the base of the mixed layer using values from oij_mld. 
-Then the script checked the downward vertical velocity (w in oijl). 
-If w(i,j,l) was positive (downward) the script moved on; 
-if w(i,j,l) was negative (upward), then w(i,j,l) was multiplied by the area of the cell 
-and added to a running sum. 
-This sum was ultimately normalized to sum of baseline Earth scenario and expressed 
-as a positive number. 
-You could normalize to one of your own simulations, doesn’t really matter. 
-
-An easier way to do this would be just to do an area-weighted sum of the upward velocities
-at a fixed depth (e.g., 200 m). We got a qualitatively similar result doing it that way. 
-(Obviously must still filter only upward velocities, otherwise the area-weighted sum should be zero...). 
-
-
-'''
-'''
-
-lon=atmo4_vars['lon']
-lat=atmo4_vars['lat']
-wind_u=atmo4_vars['usurf']
-wind_v=atmo4_vars['vsurf']
-wind_vel=np.sqrt(np.power(wind_u[:],2)+np.power(wind_v[:],2))
-
-swnow_cov_per=atmo4_vars['snowicefr']
-temp_surf=atmo4_vars['tsurf']
-p_surf=atmo4_vars['prsurf']
-
-oc4_vars=ocean.variables
-
-oc_mixl=oc4_vars['oij_mld']
-rho_surf = p_surf[:]/(temp_surf[:]*R/Mmol)
-tau=C_d*rho_surf*np.power(wind_vel[:],2)
-'''
