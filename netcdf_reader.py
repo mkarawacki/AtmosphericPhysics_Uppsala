fcacb0edbae775660b5cdcc2f214044bdeb12a6c
mkarawacki <mapkarawacki@gmail.com>
2021-10-22 19:31:28 +0200

    upwelling, windstress,mixinglayer depth and ice coverage calculations added

--- netcdf_reader.py
+++ netcdf_reader.py
@@ -21,41 +21,85 @@ aij_list=glob.glob(r"EarthRotationRate/AIJ/*.nc")
 oij_list=glob.glob(r"EarthRotationRate/OIJ/*.nc")
 oijl_list=glob.glob(r"EarthRotationRate/OIJL/*.nc")
 
+atmo_1 = netCDF4.Dataset(list(filter(re.compile('_'+str(1)).search,aij_list))[0])
+ocean_1 = netCDF4.Dataset(list(filter(re.compile('_'+str(1)).search,oij_list))[0])
+oijl_1 = netCDF4.Dataset(list(filter(re.compile('_'+str(1)).search,oijl_list))[0])
+
+atmo_vars_1=atmo_1.variables
+oc_vars_1=ocean_1.variables
+oijl_vars_1 = oijl_1.variables
+
+
+downward_v_1=oijl_vars_1['w'][:]
+oceanfr_1=atmo_vars_1['ocnfr'] #  ocean fraction
+ocfrac_land_1=oc_vars_1['oxyp'] 
+cellarea_1=ocfrac_land_1[:]/oceanfr_1[:] # grid cell area
+downward_v_1[downward_v_1>0]=0
+upwelling_1=cellarea_1*downward_v_1[0]
+upwell_sum_1=np.sum(upwelling_1)
+
+tau_1=oc_vars_1['oij_ustar']
+oc_mixl_1=np.mean(oc_vars_1['oij_mld'][:])
+
+land_1=atmo_vars_1['frac_land']
+snow_cov_per_1=atmo_vars_1['snowicefr']
+ice_1=snow_cov_per_1[:]-land_1[:]
+ice_1[ice_1<0]=0
+icecov_fr_1=np.mean(ice_1)
+
+
+upwell_arr=[1]
+tau_arr=[1]
+mld=[oc_mixl_1]
+icecov=[icecov_fr_1]
+
 #periods=list(filter(reg.search,aij_list)) 
-periods= [1,4,8,16,32,64,128,243,256]
+periods= [4,8,16,32,64,128,243,256]
 for p in periods:
     reg=re.compile('_'+str(p)).search
-    print(p,list(filter(reg,aij_list))[0],list(filter(reg,oij_list))[0],list(filter(reg,oijl_list))[0])
+    #print(p,list(filter(reg,aij_list))[0],list(filter(reg,oij_list))[0],list(filter(reg,oijl_list))[0])
     atmo = netCDF4.Dataset(list(filter(reg,aij_list))[0])
     ocean = netCDF4.Dataset(list(filter(reg,oij_list))[0])
     oijl = netCDF4.Dataset(list(filter(reg,oijl_list))[0])
     atmo_vars=atmo.variables
     oc_vars=ocean.variables
     oijl_vars = oijl.variables
-    
+    oclayers=np.linspace(0,5000,13) # ocean layers
     
    
     wind_u=atmo_vars['usurf']
     wind_v=atmo_vars['vsurf']
     wind_vel=np.sqrt(np.power(wind_u[:],2)+np.power(wind_v[:],2))
 
-    swnow_cov_per=atmo_vars['snowicefr']
+    snow_cov_per=atmo_vars['snowicefr']
+    land=atmo_vars['frac_land']
+    
     temp_surf=atmo_vars['tsurf']
     p_surf=atmo_vars['prsurf']
+    ice=snow_cov_per[:]-land[:]
+    ice[ice<0]=0
+    icecov_fr=np.mean(ice)
+    icecov.append(icecov_fr)
 
-    
-
-    oc_mixl=oc_vars['oij_mld']
+    oc_mixl=oc_vars['oij_mld'][:]
     rho_surf = p_surf[:]/(temp_surf[:]*R/Mmol)
     #tau=C_d*rho_surf*np.power(wind_vel[:],2)
-    tau=oc_vars['oij_ustar'] 
-    land=atmo_vars['frac_land']
+    tau=oc_vars['oij_ustar'] # windstress
+    
     pot_dens=oijl_vars['pot_dens']
     downward_v=oijl_vars['w'][:]
     oceanfr=atmo_vars['ocnfr'] #  ocean fraction
     ocfrac_land=oc_vars['oxyp'] 
     cellarea=ocfrac_land[:]/oceanfr[:] # grid cell area
-    
+    downward_v[downward_v>0]=0
+    upwelling=cellarea*downward_v[0]
+    upwell_sum=np.sum(upwelling)/upwell_sum_1
+    upwell_arr.append(upwell_sum)
+    tau_arr.append(np.mean(tau)/np.mean(tau_1))
+    oc_mixl=np.mean(oc_vars['oij_mld'][:])
+    mld.append(oc_mixl)
+
+
 '''
 
 Hi Mike! 
@@ -64,9 +108,12 @@ That sounds like a fun project! Happy to help.
 
 The mixed layer depth (oij_mld) 
 was unmodified and should be straightforward. 
+
+
 Sea ice was only slightly modified. 
 I just calculated sea ice cover with snowicefr (aij) 
 after applying a land mask. 
+
 I suppose oicefr (also aij) would be easier if you don’t mind some lakes. 
 
 The others panels in Figure 7 required some simple calculations that use: 
