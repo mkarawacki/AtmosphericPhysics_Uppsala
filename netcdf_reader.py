12a0ca8c511af3105ead02ce54d8035cb84a6d43
mkarawacki <mapkarawacki@gmail.com>
2021-10-22 17:47:29 +0200

    cell area problem "solved"

--- netcdf_reader.py
+++ netcdf_reader.py
@@ -32,8 +32,9 @@ for p in periods:
     atmo_vars=atmo.variables
     oc_vars=ocean.variables
     oijl_vars = oijl.variables
-    lon=atmo_vars['lon']
-    lat=atmo_vars['lat']
+    
+    
+   
     wind_u=atmo_vars['usurf']
     wind_v=atmo_vars['vsurf']
     wind_vel=np.sqrt(np.power(wind_u[:],2)+np.power(wind_v[:],2))
@@ -46,20 +47,15 @@ for p in periods:
 
     oc_mixl=oc_vars['oij_mld']
     rho_surf = p_surf[:]/(temp_surf[:]*R/Mmol)
-    tau=C_d*rho_surf*np.power(wind_vel[:],2)
-'''
-atmo = netCDF4.Dataset('EarthRotationRate/AIJ/ANN0940-0949.aijP211eoDOFP3Od_4daysg.nc')
-ocean=netCDF4.Dataset('EarthRotationRate/OIJ/ANN0940-0949.oijP211eoDOFP3Od_4daysg.nc')
-oijl=netCDF4.Dataset('EarthRotationRate/OIJL/ANN0940-0949.oijlP211eoDOFP3Od_4daysg.nc')
-
-
-atmo256 = netCDF4.Dataset('EarthRotationRate/AIJ/ANN0940-0949.aijP211eoDOFP3Od_4daysg.nc')
-ocean256=netCDF4.Dataset('EarthRotationRate/OIJ/ANN0940-0949.oijP211eoDOFP3Od_4daysg.nc')
-oijl_256=netCDF4.Dataset('EarthRotationRate/OIJL/ANN0750-0799.oijlP211eoDOFP3Od_256daysg.nc')
-atmo4_vars=atmo.variables
-keys=atmo4_vars.keys()
-#print(keys)
-'''
+    #tau=C_d*rho_surf*np.power(wind_vel[:],2)
+    tau=oc_vars['oij_ustar'] 
+    land=atmo_vars['frac_land']
+    pot_dens=oijl_vars['pot_dens']
+    downward_v=oijl_vars['w'][:]
+    oceanfr=atmo_vars['ocnfr'] #  ocean fraction
+    ocfrac_land=oc_vars['oxyp'] 
+    cellarea=ocfrac_land[:]/oceanfr[:] # grid cell area
+    
 '''
 
 Hi Mike! 
@@ -79,9 +75,20 @@ The others panels in Figure 7 required some simple calculations that use:
 The equations are provided in Section 2.4 and for the calculations are fairly straightforward, 
 the possible exception being upwelling. 
 
-For the upwelling calculation, I wrote a silly script that looped through the i,j grid and found the ocean level that contained the base of the mixed layer using values from oij_mld. Then the script checked the downward vertical velocity (w in oijl). If w(i,j,l) was positive (downward) the script moved on; if w(i,j,l) was negative (upward), then w(i,j,l) was multiplied by the area of the cell and added to a running sum. This sum was ultimately normalized to sum of baseline Earth scenario and expressed as a positive number. You could normalize to one of your own simulations, doesn’t really matter. 
+For the upwelling calculation, 
+I wrote a silly script that looped through the i,j grid and found the ocean level
+that contained the base of the mixed layer using values from oij_mld. 
+Then the script checked the downward vertical velocity (w in oijl). 
+If w(i,j,l) was positive (downward) the script moved on; 
+if w(i,j,l) was negative (upward), then w(i,j,l) was multiplied by the area of the cell 
+and added to a running sum. 
+This sum was ultimately normalized to sum of baseline Earth scenario and expressed 
+as a positive number. 
+You could normalize to one of your own simulations, doesn’t really matter. 
 
-An easier way to do this would be just to do an area-weighted sum of the upward velocities at a fixed depth (e.g., 200 m). We got a qualitatively similar result doing it that way. (Obviously must still filter only upward velocities, otherwise the area-weighted sum should be zero...). 
+An easier way to do this would be just to do an area-weighted sum of the upward velocities
+at a fixed depth (e.g., 200 m). We got a qualitatively similar result doing it that way. 
+(Obviously must still filter only upward velocities, otherwise the area-weighted sum should be zero...). 
 
 
 '''
