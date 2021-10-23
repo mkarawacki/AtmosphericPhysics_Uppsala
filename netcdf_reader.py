5d13c3a9769a3f17f579d450e51eb9923bbc1904
mkarawacki <mapkarawacki@gmail.com>
2021-10-23 21:39:33 +0200

    Row labels and tidal locking added for map grid plot

--- netcdf_reader.py
+++ netcdf_reader.py
@@ -55,52 +55,59 @@ ice_1=snow_cov_per_1[:]-land_1[:]
 ice_1[ice_1<0]=0
 icecov_fr_1=np.mean(ice_1)
 
-fig = plt.figure(600)
+fig, axs = plt.subplots(11,4, figsize=(16, 18))
+#fig.subplots_adjust(hspace = .25, wspace=.1)
+#ax = fig.add_subplot(941)
+axs = axs.ravel()
 
-ax = fig.add_subplot(941)
 
-ax.set_title("Ice coverage")
-m = Basemap(projection='moll',lat_0=0,lon_0=0)
+pad = 5 # in points
+
+
+axs[0].annotate("P = {} d".format(str(1)),xy=(0, 0.5), xytext=(-axs[0].yaxis.labelpad-pad,0), xycoords=axs[0].yaxis.label, textcoords='offset points',size='large', ha='right', va='center')
+axs[0].set_title("Ice coverage")
+m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[0])
 
 lon, lat = np.meshgrid(lons, lats)
 xi, yi = m(lon, lat)
 m.drawcoastlines()
+m.fillcontinents(color='white')
 cs = m.pcolor(xi,yi,ice_1[:],cmap='jet')
-cbar = m.colorbar(cs, location='bottom', pad="10%")
+cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
 
 
-ax = fig.add_subplot(942)
-ax.set_title("Upwelling")
-m = Basemap(projection='moll',lat_0=0,lon_0=0)
+#axs[2] = fig.add_subplot(942)
+axs[1].set_title("Upwelling")
+m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[1])
 m.drawcoastlines()
+m.fillcontinents(color='white')
 lon, lat = np.meshgrid(lons, lats)
 xi, yi = m(lon, lat)
 
 cs = m.pcolor(xi,yi,np.squeeze(upwelling_1[:]),cmap='jet')
-cbar = m.colorbar(cs, location='bottom', pad="10%")
+cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
 
-ax = fig.add_subplot(943)
-ax.set_title("Wind stress")
-m = Basemap(projection='moll',lat_0=0,lon_0=0)
+#ax = fig.add_subplot(943)
+axs[2].set_title("Wind stress")
+m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[2])
 m.drawcoastlines()
+m.fillcontinents(color='white')
 lon, lat = np.meshgrid(lons, lats)
 xi, yi = m(lon, lat)
 
 cs = m.pcolor(xi,yi,np.squeeze(tau_1[:]),cmap='jet')
-cbar = m.colorbar(cs, location='bottom', pad="10%")
+cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
 
-ax = fig.add_subplot(944)
-ax.set_title("Mixing layer depth")
-m = Basemap(projection='moll',lat_0=0,lon_0=0)
+#ax = fig.add_subplot(944)
+axs[3].set_title("Mixing layer depth")
+m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[3])
 m.drawcoastlines()
+m.fillcontinents(color='white')
 lon, lat = np.meshgrid(lons, lats)
 xi, yi = m(lon, lat)
 
 cs = m.pcolor(xi,yi,oc_vars_1['oij_mld'][:],cmap='jet')
-cbar = m.colorbar(cs, location='bottom', pad="10%")
-
-
-plt.show()
+cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
 
 
 upwell_arr=[1]
@@ -110,8 +117,10 @@ icecov=[icecov_fr_1]
 
 #periods=list(filter(reg.search,aij_list)) 
 periods= [4,8,16,32,64,128,243,256]
-i=4
+col=3
+
 for p in periods:
+    
     reg=re.compile('_'+str(p)).search #Regular Expression looking for string matching period duration in .nc filenames
     atmo = netCDF4.Dataset(list(filter(reg,aij_list))[0]) #read AIJ .nc file with given period
     ocean = netCDF4.Dataset(list(filter(reg,oij_list))[0]) #read OIJ .nc file with given period
@@ -153,7 +162,197 @@ for p in periods:
     tau_arr.append(np.mean(tau)/np.mean(tau_1))
     oc_mixl=np.mean(oc_vars['oij_mld'][:])
     mld.append(oc_mixl)
-    i=i+1
-    ax = fig.add_subplot(9,4,4+i)
+    
+    axs[col+1].annotate("P = {} d".format(str(p)),xy=(0, 0.5), xytext=(-axs[col].yaxis.labelpad-pad,0), xycoords=axs[col+1].yaxis.label, textcoords='offset points',size='large', ha='right', va='center')
+    m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[col+1])
+    lon, lat = np.meshgrid(lons, lats)
+    xi, yi = m(lon, lat)
+    m.drawcoastlines()
+    m.fillcontinents(color='white')
+    cs = m.pcolor(xi,yi,ice[:],cmap='jet')
+    cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
     
 
+    m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[col+2])
+    m.drawcoastlines()
+    m.fillcontinents(color='white')
+    lon, lat = np.meshgrid(lons, lats)
+    xi, yi = m(lon, lat)
+
+    cs = m.pcolor(xi,yi,np.squeeze(upwelling[:]),cmap='jet')
+    cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
+
+
+    m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[col+3])
+    m.drawcoastlines()
+    m.fillcontinents(color='white')
+    lon, lat = np.meshgrid(lons, lats)
+    xi, yi = m(lon, lat)
+
+    cs = m.pcolor(xi,yi,np.squeeze(tau[:]),cmap='jet')
+    cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
+
+    
+    m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[col+4])
+    m.drawcoastlines()
+    m.fillcontinents(color='white')
+    lon, lat = np.meshgrid(lons, lats)
+    xi, yi = m(lon, lat)
+
+    cs = m.pcolor(xi,yi,oc_vars['oij_mld'][:],cmap='jet')
+    cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
+    
+    col=col+4
+'''
+tidal locking - P = 365 d
+'''    
+atmo_365 = netCDF4.Dataset(list(filter(re.compile('_'+str(365)).search,aij_list))[0])
+ocean_365 = netCDF4.Dataset(list(filter(re.compile('_'+str(365)).search,oij_list))[0])
+oijl_365 = netCDF4.Dataset(list(filter(re.compile('_'+str(365)).search,oijl_list))[0])
+
+atmo_vars_365=atmo_365.variables
+oc_vars_365=ocean_365.variables
+oijl_vars_365 = oijl_365.variables
+
+lons=atmo_vars_365['lon'][:]
+lats=atmo_vars_365['lat'][:]
+
+
+
+downward_v_365=oijl_vars_365['w'][:]
+oceanfr_365=atmo_vars_365['ocnfr'] #  ocean fraction
+ocfrac_land_365=oc_vars_365['oxyp'] 
+cellarea_365=ocfrac_land_365[:]/oceanfr_365[:] # grid cell area
+downward_v_365[downward_v_365>0]=0
+upwelling_365=cellarea_365*downward_v_365[0]
+upwell_sum_365=np.sum(upwelling_365)
+
+tau_365=oc_vars_365['oij_ustar']
+oc_mixl_365=np.mean(oc_vars_365['oij_mld'][:])
+
+land_365=atmo_vars_365['frac_land']
+snow_cov_per_365=atmo_vars_365['snowicefr']
+ice_365=snow_cov_per_365[:]-land_365[:]
+ice_365[ice_365<0]=0
+icecov_fr_365=np.mean(ice_365)
+
+axs[col+1].annotate("P = {} d".format(str(365)),xy=(0, 0.5), xytext=(-axs[col+1].yaxis.labelpad-pad,0), xycoords=axs[col+1].yaxis.label, textcoords='offset points',size='large', ha='right', va='center')
+
+m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[col+1])
+
+lon, lat = np.meshgrid(lons, lats)
+xi, yi = m(lon, lat)
+m.drawcoastlines()
+m.fillcontinents(color='white')
+cs = m.pcolor(xi,yi,ice_365[:],cmap='jet')
+cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
+
+
+m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[col+2])
+m.drawcoastlines()
+m.fillcontinents(color='white')
+lon, lat = np.meshgrid(lons, lats)
+xi, yi = m(lon, lat)
+
+cs = m.pcolor(xi,yi,np.squeeze(upwelling_365[:]),cmap='jet')
+cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
+
+
+m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[col+3])
+m.drawcoastlines()
+m.fillcontinents(color='white')
+lon, lat = np.meshgrid(lons, lats)
+xi, yi = m(lon, lat)
+
+cs = m.pcolor(xi,yi,np.squeeze(tau_365[:]),cmap='jet')
+cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
+
+
+m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[col+4])
+m.drawcoastlines()
+m.fillcontinents(color='white')
+lon, lat = np.meshgrid(lons, lats)
+xi, yi = m(lon, lat)
+
+cs = m.pcolor(xi,yi,oc_vars_365['oij_mld'][:],cmap='jet')
+cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
+
+'''
+tidal locking - P = 365 d, 0 obliquity
+'''    
+atmo_365_0 = netCDF4.Dataset(list(filter(re.compile('_'+str(365)+'daysg0o').search,aij_list))[0])
+ocean_365_0 = netCDF4.Dataset(list(filter(re.compile('_'+str(365)+'daysg0o').search,oij_list))[0])
+oijl_365_0 = netCDF4.Dataset(list(filter(re.compile('_'+str(365)+'daysg0o').search,oijl_list))[0])
+
+atmo_vars_365_0=atmo_365_0.variables
+oc_vars_365_0=ocean_365_0.variables
+oijl_vars_365_0 = oijl_365_0.variables
+
+lons=atmo_vars_365_0['lon'][:]
+lats=atmo_vars_365_0['lat'][:]
+
+
+
+downward_v_365_0=oijl_vars_365_0['w'][:]
+oceanfr_365_0=atmo_vars_365_0['ocnfr'] #  ocean fraction
+ocfrac_land_365_0=oc_vars_365_0['oxyp'] 
+cellarea_365_0=ocfrac_land_365_0[:]/oceanfr_365_0[:] # grid cell area
+downward_v_365_0[downward_v_365_0>0]=0
+upwelling_365_0=cellarea_365_0*downward_v_365_0[0]
+upwell_sum_365_0=np.sum(upwelling_365_0)
+
+tau_365_0=oc_vars_365_0['oij_ustar']
+oc_mixl_365_0=np.mean(oc_vars_365_0['oij_mld'][:])
+
+land_365_0=atmo_vars_365_0['frac_land']
+snow_cov_per_365_0=atmo_vars_365_0['snowicefr']
+ice_365_0=snow_cov_per_365_0[:]-land_365_0[:]
+ice_365_0[ice_365_0<0]=0
+icecov_fr_365_0=np.mean(ice_365_0)
+
+
+axs[col+5].annotate("P = {} d, $\epsilon=0$".format(str(365)),xy=(0, 0.5), xytext=(-axs[col+5].yaxis.labelpad-pad,0), xycoords=axs[col+5].yaxis.label, textcoords='offset points',size='large', ha='right', va='center')
+
+m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[col+5])
+
+lon, lat = np.meshgrid(lons, lats)
+xi, yi = m(lon, lat)
+m.drawcoastlines()
+m.fillcontinents(color='white')
+cs = m.pcolor(xi,yi,ice_365_0[:],cmap='jet')
+cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
+
+
+m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[col+6])
+m.drawcoastlines()
+m.fillcontinents(color='white')
+lon, lat = np.meshgrid(lons, lats)
+xi, yi = m(lon, lat)
+
+cs = m.pcolor(xi,yi,np.squeeze(upwelling_365_0[:]),cmap='jet')
+cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
+
+
+m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[col+7])
+m.drawcoastlines()
+m.fillcontinents(color='white')
+lon, lat = np.meshgrid(lons, lats)
+xi, yi = m(lon, lat)
+
+cs = m.pcolor(xi,yi,np.squeeze(tau_365_0[:]),cmap='jet')
+cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
+
+m = Basemap(projection='moll',lat_0=0,lon_0=0,ax=axs[col+8])
+m.drawcoastlines()
+m.fillcontinents(color='white')
+lon, lat = np.meshgrid(lons, lats)
+xi, yi = m(lon, lat)
+
+cs = m.pcolor(xi,yi,oc_vars_365_0['oij_mld'][:],cmap='jet')
+cbar = m.colorbar(cs, location='bottom',size="5%", pad='2%')
+#plt.savefig("ParameterGlobalMaps.png",dpi=1200)    
+plt.show()
+'''
+mean values' scatterplots
+'''
+periods1=[1]+periods+[365]
